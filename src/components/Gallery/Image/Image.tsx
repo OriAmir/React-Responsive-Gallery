eb192b094541968fa075ba6245d4ed029480da73
OriAmir <oriamir1@gmail.com>
2022-06-21 21:03:00 +0300

    chg: add img alt option, small code changes (#21)
    chg: add img alt option, small code changes

--- src/components/Gallery/Image/Image.tsx
+++ src/components/Gallery/Image/Image.tsx
@@ -1,24 +1,30 @@
 import styled, { css } from "styled-components";
 
 type ImgProps = {
-  imageSrc: string;
-  imgMaxWidth: number;
+  src: string;
+  maxWidth: number;
+  alt?: string;
   paddingBottom: number;
   className?: string;
   useLightBox?: boolean;
   onClick?: () => void;
 };
 
-const Image = styled.img.attrs<ImgProps>(({ imageSrc }) => ({
-  src: imageSrc,
+const Image = styled.img.attrs<ImgProps>(({ src }) => ({
+  src,
 }))<ImgProps>`
-  max-width: ${({ imgMaxWidth }) => imgMaxWidth}%;
+  max-width: ${({ maxWidth }) => maxWidth}%;
   height: auto;
   margin-bottom: ${({ paddingBottom }) => paddingBottom || 0}px;
   ${({ useLightBox = false }) =>
     useLightBox &&
     css`
       cursor: pointer;
+    `}
+  ${({ alt = "" }) =>
+    alt &&
+    css`
+      alt: ${alt};
     `}
 `;
 
