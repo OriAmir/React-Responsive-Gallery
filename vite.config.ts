3fce4d58ec031253c9d7c39dc95f78011ffcf138
Ori Amir <oriamir@Oris-MacBook-Pro.local>
2023-02-20 23:14:19 +0200

    change configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import tsConfigPaths from "vite-tsconfig-paths";
-import libCss from "vite-plugin-libcss";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import eslint from "vite-plugin-eslint";
 
 export default defineConfig((configEnv) => ({
@@ -14,18 +14,19 @@ export default defineConfig((configEnv) => ({
     eslint({
       failOnWarning: true,
     }),
-    libCss(),
     react(),
     tsConfigPaths(),
     dts({
       outputDir: "dist/declarations",
       insertTypesEntry: true,
     }),
+    cssInjectedByJsPlugin(),
   ],
   build: {
-    // sourcemap: true --> for dev debug,
     cssCodeSplit: true,
+    // sourcemap: true --> for dev debug,
     lib: {
+      formats: ["es"],
       entry: resolve("src", "index.tsx"),
       fileName: "react-responsive-gallery",
       name: "ReactResponsiveGallery",
